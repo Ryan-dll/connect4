cae3ee7857c4c9e56ee68c516f7f391abb4ccf77
Shreesha Maddur <maddurs1@msu.edu>
2021-03-05 10:54:57 -0500

    Added two buttons in GamePlayActivity

--- app/src/main/java/edu/msu/weath151/connect4/GamePlayActivity.java
+++ app/src/main/java/edu/msu/weath151/connect4/GamePlayActivity.java
@@ -2,21 +2,37 @@ package edu.msu.weath151.connect4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.view.View;
 
 public class GamePlayActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
+
+
     }
 
     public void onEndGame(View view) {
         Intent intent = new Intent(this, GameEndActivity.class);
         startActivity(intent);
+    }
+
+    public void greenPiece(View view) {
+
+    }
+
+
+    public void whitePiece(View view) {
+
     }
 
 
