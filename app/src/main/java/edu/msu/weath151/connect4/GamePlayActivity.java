8d293025d0c15f9b9143d4df0565c67439cd3a77
Ben Weatherford <weath151@msu.edu>
2021-03-19 18:07:49 -0400

    MainActivity passes names to GamePlayActivity and GamePlayActivity displays each name

--- app/src/main/java/edu/msu/weath151/connect4/GamePlayActivity.java
+++ app/src/main/java/edu/msu/weath151/connect4/GamePlayActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class GamePlayActivity extends AppCompatActivity {
 
@@ -15,7 +16,13 @@ public class GamePlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
 
-
+        Intent intent = getIntent();
+        String player1Name = intent.getStringExtra(MainActivity.NAME1);
+        String player2Name = intent.getStringExtra(MainActivity.NAME2);
+        TextView player1 = (TextView)findViewById(R.id.textView);
+        TextView player2 = (TextView)findViewById(R.id.textView2);
+        player1.setText(player1Name);
+        player2.setText(player2Name);
     }
 
     public void onEndGame(View view) {
--- app/src/main/java/edu/msu/weath151/connect4/MainActivity.java
+++ app/src/main/java/edu/msu/weath151/connect4/MainActivity.java
@@ -15,15 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private final String NAME1 = "name1";
-    private final String NAME2 = "name2";
+    public static final String NAME1 = "name1";
+    public static final String NAME2 = "name2";
 
     public void onStartGame(View view) {
         Intent intent = new Intent(this, GamePlayActivity.class);
         EditText editText1 = findViewById(R.id.EditText1);
         EditText editText2 = findViewById(R.id.EditText2);
-        intent.putExtra(NAME1, editText1.getText());
-        intent.putExtra(NAME2, editText2.getText());
+        intent.putExtra(NAME1, editText1.getText().toString());
+        intent.putExtra(NAME2, editText2.getText().toString());
         startActivity(intent);
     }
 
