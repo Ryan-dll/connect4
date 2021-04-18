6fa9cb4d7c2251fcc4f5bc6dea04b065afdabd7a
shorery1 <shorery1@cse.msu.edu>
2021-04-18 13:42:12 -0400

    Updated Login UI and Lobby UI. Made dialogue boxes for active games and new account creation.

--- app/src/main/java/edu/msu/weath151/connect4/LobbyDlg.java
+++ app/src/main/java/edu/msu/weath151/connect4/LobbyDlg.java
@@ -12,18 +12,19 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
-public class LoginDlg extends DialogFragment {
+public class LobbyDlg extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
 
         // Get the layout inflater
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
         // Pass null as the parent view because its going in the dialog layout
         @SuppressLint("InflateParams")
-        View view = inflater.inflate(R.layout.login_dlg, null);
+        View view = inflater.inflate(R.layout.join_game_dlg, null);
         builder.setView(view);
 
         // Add a cancel button
